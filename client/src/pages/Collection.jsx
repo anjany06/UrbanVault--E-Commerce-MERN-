12d8fbf3aa32c9bd37451b209f3880b6928c99de
Anjany Pandey <anjany.pandey06@gmail.com>
2024-12-25 02:40:01 +0530

    collection page filter ui completed with responsive also

--- client/src/pages/Collection.jsx
+++ client/src/pages/Collection.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import assets from "../assets/assets/frontend_assets/assets";
 
 const Collection = () => {
+  const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
   return (
-    <div>Collection</div>
-  )
-}
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+      {/* Filter options */}
+      <div className="min-w-60">
+        <p
+          onClick={() => setShowFilter(!showFilter)}
+          className="my-2 text-xl flex items-center cursor-pointer gap-2"
+        >
+          FILTERS
+          <img
+            src={assets.dropdown_icon}
+            className={`h-3 sm:hidden ${showFilter ? "rotate-90" : ""}`}
+            alt=""
+          />
+        </p>
+        {/* category Filter */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 mt-6 ${
+            showFilter ? "" : "hidden"
+          } sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium">CATEGORY</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"MEN"} />
+              Men
+            </p>
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"Women"} />
+              Women
+            </p>
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"Kids"} />
+              Kids
+            </p>
+          </div>
+        </div>
+        {/* SUbCategory filter */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 my-5 ${
+            showFilter ? "" : "hidden"
+          } sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium">TYPE</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"Topwear"} />
+              Topwear
+            </p>
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"Bottomwear"} />
+              Bottomwear
+            </p>
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"winterwear"} />
+              Winterwear
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Collection
+export default Collection;
