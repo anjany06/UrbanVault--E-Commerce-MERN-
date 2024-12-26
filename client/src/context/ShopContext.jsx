1b256c5938693abd46825bda765f6677aa57c02e
Anjany Pandey <anjany.pandey06@gmail.com>
2024-12-27 02:00:06 +0530

    `Update Navbar component to display cart count from ShopContext`

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -34,9 +34,24 @@ const ShopContextProvider = (props) => {
     setCartItems(cartData);
   };
 
-  useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {}
+      }
+    }
+    console.log(totalCount);
+    return totalCount;
+  };
+
+  // useEffect(() => {
+  //   console.log(cartItems);
+  // }, [cartItems]);
 
   const value = {
     products,
@@ -48,6 +63,7 @@ const ShopContextProvider = (props) => {
     setShowSearch,
     cartItems,
     addToCart,
+    getCartCount,
   };
 
   return (
