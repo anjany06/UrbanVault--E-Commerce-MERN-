03bb1dfbf17c7df3619c290bf8115315572c1dc9
Anjany Pandey <anjany.pandey06@gmail.com>
2024-12-27 16:51:57 +0530

    `Added getCartAmount function and CartTotal component to ShopContext and Cart page respectively`

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -2,6 +2,8 @@ import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title";
 import assets from "../assets/assets/frontend_assets/assets";
+import CartTotal from "../components/CartTotal";
+
 const Cart = () => {
   const { products, currency, cartItems, updateQuantity } =
     useContext(ShopContext);
@@ -65,8 +67,15 @@ const Cart = () => {
                 type="number"
                 min={1}
                 defaultValue={item.quantity}
-                onChange={(e)=>e.target.value === '' || e.target.value === '0' ? null :updateQuantity(item._id, item.size, Number(e.target.value))}
-
+                onChange={(e) =>
+                  e.target.value === "" || e.target.value === "0"
+                    ? null
+                    : updateQuantity(
+                        item._id,
+                        item.size,
+                        Number(e.target.value)
+                      )
+                }
               />
               <img
                 onClick={() => updateQuantity(item._id, item.size, 0)}
@@ -77,6 +86,11 @@ const Cart = () => {
             </div>
           );
         })}
+      </div>
+      <div className="flex justify-end my-20">
+        <div className="w-full sm:w-[450px]">
+          <CartTotal />
+        </div>
       </div>
     </div>
   );
