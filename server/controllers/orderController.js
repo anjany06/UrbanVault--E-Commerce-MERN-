c45e1564204e6f1a0659cdfc2dadda3cc189eedb
Anjany Pandey <anjany.pandey06@gmail.com>
2024-12-30 21:53:21 +0530

    order

---
+++ server/controllers/orderController.js
@@ -0,0 +1,30 @@
+
+
+//placing order using COD method
+const placeOrder = async(req, res)=>{
+
+}
+//placing order using Stripe method
+const placeOrderStripe = async(req, res)=>{
+  
+}
+
+//placing order using razorpay method
+const placeOrderRazorpay = async(req, res)=>{
+  
+}
+
+//All orders data for admin panel
+const allOrders = async(req,res) =>{
+
+}
+
+// User orders data for Frontend
+const userOrders = async(req,res)=>{
+
+}
+
+//Update order Status
+const updateStatus = async(req,res)=>{
+  
+}
