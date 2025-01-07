be4742b7d5390bcdbf9de77a42be01bc4e2f6cbe
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-07 21:24:02 +0530

    nodemailer

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -5,7 +5,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
-  secure: false,
+  secure: false, // true for port 465, false for other ports
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
