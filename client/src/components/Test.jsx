60b440e6d279b01392d295c8455426f435a0346c
Anjany Pandey <anjany.pandey06@gmail.com>
2026-02-28 21:10:54 +0530

    add Test component with placeholder content

---
+++ client/src/components/Test.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
+      laudantium ducimus ut velit sunt at incidunt, quas possimus, fugit hic
+      minima, asperiores aperiam suscipit cupiditate porro iusto fuga? At,
+      nobis.
+    </div>
+  );
+};
+
+export default Test;
